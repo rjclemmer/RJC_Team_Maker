741a7140741cb5528f40bdbfd2152c18abdadea9
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-01-22 12:48:00 -0500

    addIntern, addEngineer functions, more questions, and createTeam function. Also made js files in /lib

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const manager = [
         validate: validateInput,
     },
 ];
-
+// engineer questions
 const engineer = [
     {
         type: 'input',
@@ -91,19 +91,83 @@ const engineer = [
     },
 
     
-]
+];
 
+// add engineer function
+function addEngineer() {
+    inquirer.prompt(engineer)
+    .then((data) => {
+        team.push(data.name, data.id, data.email, data.github);
+    })
+}
+// intern questions
+const intern = [
+    {
+        type: 'input',
+        message: "Intern's name?",
+        name: 'name',
+        default: 'Intern',
+        validate: validateInput,
 
+    },
+    {
+        type: 'input',
+        message: "Intern's ID#?",
+        name: 'id',
+        default: '003',
+        // if id isNAN, console.log
+        validate: id => {
+            if(isNaN(id)) {
+                console.log("Please return a numeric ID#")
+            } else {
+                return true;
+            }
+        },
+
+    },
+    {
+        type: 'input',
+        message: "Intern's email?",
+        name: 'email',
+        default: 'default@default.com',
+        validate: validateInput,
+
+    },
+    {
+        type: 'input',
+        message: "Intern's school?",
+        name: 'school',
+        default: 'School of Hard Knocks',
+        validate: validateInput,
+    },
+];
+
+// add intern function
+
+function addIntern() {
+    inquirer.prompt(intern)
+    .then((data) => {
+        team.push(data.name, data.id, data.email, data.school);
+    })
+};
+
+// create team function
+function createTeam() {
+    addIntern();
+    addEngineer();
+};
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(manager)
+    
 
 
 //console logs input
     .then((data) => {
+        team.push(data.name, data.id, data.email, data.officeNumber);
        // writeToFile('yourREADME.md', generateMarkdown(data));
-        
-        console.log(data);
+        createTeam();
+        console.log(team);
     });
     
 }
