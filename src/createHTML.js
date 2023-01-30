1a97a8f61d3f441108e1ea8342579ee929e9bbff
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-01-30 00:11:36 -0500

    added engineer

--- src/createHTML.js
+++ src/createHTML.js
@@ -6,6 +6,23 @@ function createHTML(data) {
     console.log(data);
     var content =``;
     content = `<p>${data}</p>`;
+
+    for (let i =1; i < data.length; i++) {
+        if (data[i].role === "Engineer") {
+            content += `
+            <ul>
+            <li>Name: ${data[i].getName()}</li>
+            <li>Role: ${data[i].getRole()}</li>
+            <li>ID: ${data[i].getId()}</li>
+            <li>Email: ${data[i].getEmail()}</li>
+            <li>Github: ${data[i].getGithub()}</li>
+        </ul>
+            `
+        }
+        
+    };
+
+    //this displays the manager's content
     content +=`    
     <ul>
     <li>Name: ${data[0].getName()}</li>
