51e2bcd8a6dd7eabe312e297a11e90754a77e22f
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-02-05 20:52:03 -0500

    employee tests!

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,16 +1,23 @@
 const Employee = require('../lib/Employee');
-
-test("create Employee", () => {
-    const emp = new Employee();
-    expect(typeof(emp)).toBe("object");
+const emp = new Employee("Robert Clemmer","001","robert@test.com");
+test("constructor returns name, id #, and email of employee", () => {
+    expect(emp.name).toBe("Robert Clemmer");
+    expect(emp.id).toBe("001");
+    expect(emp.email).toBe('robert@test.com');
 });
 
-// describe('Employee', () => {
-//     it('test if we can get the employee name', () => {
-//         const value = 'Robbie Clem';
-//         const emp = new Employee(value, 3, 'robbie@clem.com');
-//         expect(emp.getName()).toBe(value);
-//     });
-// }
+test("getName() returns employee name", () => {
+    expect(emp.getName()).toBe("Robert Clemmer");
+});
 
-// );
+test("getId() returns employee Id", () => {
+    expect(emp.getId()).toBe("001");
+});
+
+test("getEmail) returns employee email", () => {
+    expect(emp.getEmail()).toBe("robert@test.com");
+});
+
+test("getRole() returns employee role", () => {
+    expect(emp.getRole()).toBe("Employee");
+});
