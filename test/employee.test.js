1f6df7b51c700c98eb466cc1a59f5c2ea0deeaef
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-02-05 20:35:37 -0500

    yay! scripts! tests!

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,11 +1,16 @@
 const Employee = require('../lib/Employee');
 
-describe('Employee', () => {
-    it('test if we can get the employee name', () => {
-        const value = 'Robbie Clem';
-        const emp = new Employee(value, 3, 'robbie@clem.com');
-        expect(emp.getName()).toBe(value);
-    });
-}
+test("create Employee", () => {
+    const emp = new Employee();
+    expect(typeof(emp)).toBe("object");
+});
 
-);
+// describe('Employee', () => {
+//     it('test if we can get the employee name', () => {
+//         const value = 'Robbie Clem';
+//         const emp = new Employee(value, 3, 'robbie@clem.com');
+//         expect(emp.getName()).toBe(value);
+//     });
+// }
+
+// );
